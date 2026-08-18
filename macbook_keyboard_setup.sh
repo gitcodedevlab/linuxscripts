b9e641ed05902671907d4483b71925f77c18bd14
gitcodedevlab <codedevlab@proton.me>
2026-08-18 20:57:58 -0300

    Increase keyboard repeat delay from 0.05 to 0.2

--- macbook_keyboard_setup.sh
+++ macbook_keyboard_setup.sh
@@ -41,7 +41,7 @@ KBD_LIGHT="/sys/class/leds/smc::kbd_backlight"
 MAX_KBD=$(cat $KBD_LIGHT/max_brightness)
 STEP_KBD=25
 
-REPEAT_DELAY=0.05
+REPEAT_DELAY=0.2
 
 dec_display() {
     CURRENT=$(cat $BACKLIGHT/brightness)
