7300d5b70b9b0b391e4e95c823c8e8df84ea84ec
gitcodedevlab <codedevlab@proton.me>
2026-08-18 20:50:51 -0300

    Update macbook_keyboard_setup.sh

--- macbook_keyboard_setup.sh
+++ macbook_keyboard_setup.sh
@@ -33,13 +33,13 @@ cat > /usr/local/bin/brightness-control << 'SCRIPT'
 
 BACKLIGHT="/sys/class/backlight/intel_backlight"
 MAX_DISPLAY=$(cat $BACKLIGHT/max_brightness)
-STEP_DISPLAY=100
+STEP_DISPLAY=180
 
 KBD_LIGHT="/sys/class/leds/smc::kbd_backlight"
 MAX_KBD=$(cat $KBD_LIGHT/max_brightness)
-STEP_KBD=30
+STEP_KBD=25
 
-REPEAT_DELAY=0.2
+REPEAT_DELAY=0.1
 
 dec_display() {
     CURRENT=$(cat $BACKLIGHT/brightness)
